716c2437b00d49c7abfa55e22df9cb855e655ce2
sarjun12 <mohamedsarjun@proton.me>
2024-02-18 19:06:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function ageInDays(){
+var birthYear = prompt('which year you were born...my friend?');
+var agedays =(2023 - birthYear) * 365;
+var h1 = document.createElement('h1');
+var textAnswer = document.createTextNode('you are' + agedays + 'days old');
+h1.setAttribute('id','agedays');
+h1.appendChild(textAnswer);
+document.getElementById('flex-box-result').appendchild(h1);
+}
